691fd230ee28ec06996666c88e9221b0146f4691
KMONTH <1070796004@qq.com>
2024-08-14 00:32:00 +0800

    使用互斥锁解决高并发场景下引起的缓存击穿问题

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,6 +1,7 @@
 package com.hmdp.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -36,34 +37,51 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
     @Override
     public Result queryById(Long id) {
         String key = CACHE_SHOP_KEY + id;
-        //1.从redis查看缓存
-        String shopJson = stringRedisTemplate.opsForValue().get(key);
-        //2.判断缓存是否存在
-        if (StrUtil.isNotBlank(shopJson)) {
-            return Result.ok(JSON.parseObject(shopJson, Shop.class));
+        String lock = LOCK_SHOP_KEY + id;
+        Shop shop = null;
+        String shopJson = null;
+        try {
+            //解决缓存穿透也可以用递归,此处用迭代
+            while (true) {
+                //1.从redis查看缓存
+                shopJson = stringRedisTemplate.opsForValue().get(key);
+                //2.判断缓存是否存在,不存在就向数据库查询
+                if (StrUtil.isNotBlank(shopJson)) {
+                    return Result.ok(JSON.parseObject(shopJson, Shop.class));
+                }
+                //防止缓存穿透
+                if (shopJson != null) {
+                    return Result.fail("商户不存在");
+                }
+                //防止缓存击穿使用互斥锁
+                if (getLock(lock)) {
+                    break;
+                }
+                Thread.sleep(50);
+            }
+            //如果你确定查询的是主键字段，getById(id) 通常会比 query().eq("id", id).one() 更高效，因为它是针对主键的优化查询。
+            //query().eq("id", id).one() 在没有主键索引或复杂查询条件下可能会稍慢。
+            shop = getById(id);
+            //模拟重建延时
+            Thread.sleep(200);
+            //3.不存在返回错误
+            if (shop == null) {
+                //防止缓存穿透,向redis保存空数据
+                stringRedisTemplate.opsForValue().set(key, "", CACHE_NULL_TTL, TimeUnit.MINUTES);
+                return Result.fail("商户不存在");
+            }
+            //4.存在保存至redis并返回数据
+            shopJson = JSON.toJSONString(shop);
+            stringRedisTemplate.opsForValue().set(key, shopJson, CACHE_SHOP_TTL, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }finally {
+            unLock(lock);
         }
-        //防止缓存穿透
-        if (shopJson != null) {
-            return Result.fail("商户不存在");
-        }
-
-        //如果你确定查询的是主键字段，getById(id) 通常会比 query().eq("id", id).one() 更高效，因为它是针对主键的优化查询。
-        //query().eq("id", id).one() 在没有主键索引或复杂查询条件下可能会稍慢。
-        Shop shop = /*query().eq("id", id).one();*/ getById(id);
-
-        //3.不存在返回错误
-        if (shop == null) {
-            //防止缓存穿透,向redis保存空数据
-            stringRedisTemplate.opsForValue().set(key,"", CACHE_NULL_TTL, TimeUnit.MINUTES);
-            return Result.fail("商户不存在");
-        }
-        //4.存在保存至redis并返回数据
-        shopJson = JSON.toJSONString(shop);
-        stringRedisTemplate.opsForValue().set(key, shopJson, CACHE_SHOP_TTL, TimeUnit.MINUTES);
         return Result.ok(shop);
     }
-    //根据id修改商户
 
+    //根据id修改商户
     @Transactional
     @Override
     public Result update(Shop shop) {
@@ -74,5 +92,15 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         updateById(shop);
         stringRedisTemplate.delete(CACHE_SHOP_KEY + shop.getId());
         return Result.ok();
+    }
+
+    //用于缓存击穿互斥锁的获取
+    public boolean getLock(String key) {
+        Boolean lock = stringRedisTemplate.opsForValue().setIfAbsent(key, "lock", 10, TimeUnit.MINUTES);
+        return BooleanUtil.isTrue(lock);
+    }
+
+    public void unLock(String key) {
+        stringRedisTemplate.delete(key);
     }
 }
