e34ad5a666fad8eddcc92e27187392b12239780f
KMONTH <1070796004@qq.com>
2024-08-13 18:08:50 +0800

    对商户以及商户类别使用redis进行缓存,提高查询效率

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,5 +1,8 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.StrUtil;
+import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
@@ -27,6 +30,27 @@ import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
 @Service
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
 
-
-
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+    @Override
+    public Result queryById(Long id) {
+        String key = CACHE_SHOP_KEY+id;
+        //1.从redis查看缓存
+        String shopJson = stringRedisTemplate.opsForValue().get(key);
+        //2.判断缓存是否存在
+        if (StrUtil.isNotBlank(shopJson)) {
+            return Result.ok(JSON.parseObject(shopJson, Shop.class));
+        }
+        //如果你确定查询的是主键字段，getById(id) 通常会比 query().eq("id", id).one() 更高效，因为它是针对主键的优化查询。
+        //query().eq("id", id).one() 在没有主键索引或复杂查询条件下可能会稍慢。
+        Shop shop = /*query().eq("id", id).one();*/ getById(id);
+        //3.不存在返回错误
+        if(shop == null){
+            return Result.fail("商户不存在");
+        }
+        //4.存在保存至redis并返回数据
+        shopJson = JSON.toJSONString(shop);
+        stringRedisTemplate.opsForValue().set(key, shopJson);
+        return Result.ok(shop);
+    }
 }
--- src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,14 +1,24 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+import static com.hmdp.utils.RedisConstants.CAHE_SHOPTYPE_KEY;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author 虎哥
@@ -16,5 +26,24 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
-
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+    @Override
+    public Result queryAll() {
+        String key = CAHE_SHOPTYPE_KEY;
+        //1.从redis中查询数据,有返回,无查数据库
+        String shopTypeJson = stringRedisTemplate.opsForValue().get(key);
+        if (StrUtil.isNotBlank(shopTypeJson)) {
+            return Result.ok(JSON.parseObject(shopTypeJson,
+                    new TypeReference<List<ShopType>>(){}));
+        }
+        //2.查数据库,有保存至redis并返回,无就报错
+        List<ShopType> shopTypes = query().orderByAsc("sort").list();
+        if(shopTypes == null){
+            return Result.fail("无任何商品类型");
+        }
+        shopTypeJson = JSON.toJSONString(shopTypes);
+        stringRedisTemplate.opsForValue().set(key, shopTypeJson);
+        return Result.ok(shopTypes);
+    }
 }
