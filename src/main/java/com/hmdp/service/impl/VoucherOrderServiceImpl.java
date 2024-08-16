5bdc5a0fd674318cadebcba1729b84872fc71eda
KMONTH <1070796004@qq.com>
2024-08-16 17:56:41 +0800

    添加乐观锁防止超卖问题,并限制一人一单

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -28,7 +28,7 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
     //涉及两张表 添加事务
     @Transactional
     @Override
-    public Result getSeckillVoucher(Long voucherId) {
+    public Result   getSeckillVoucher(Long voucherId) {
         //1.根据id获取优惠券信息
         SeckillVoucher seckillVoucher = seckillVoucherService.getById(voucherId);
         if (seckillVoucher == null) {
@@ -39,16 +39,23 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         if (now.isAfter(seckillVoucher.getEndTime()) || now.isBefore(seckillVoucher.getBeginTime())) {
             return Result.fail("优惠券不在时效内");
         }
+        Long userId = UserHolder.getUser().getId();
+        if(query().eq("user_id",userId).one()!=null){
+            return Result.fail("你已达到购买限额");
+        }
         //3.判断库存是否充足
         int stock = seckillVoucher.getStock();
         if (stock <= 0) {
             return Result.fail("优惠券已被抢完");
         }
         //4.扣减库存
-        seckillVoucher.setStock(stock--);
         boolean success = seckillVoucherService.update()
                 .setSql("stock = stock - 1")
-                .eq("voucher_id", voucherId).update();
+                .eq("voucher_id", voucherId).
+                gt("stock",0).update();
+        if(success!=true){
+            return Result.fail("优惠券已被抢完!");
+        }
         //5.创建订单
         VoucherOrder voucherOrder = new VoucherOrder();
         voucherOrder.setId(redisWorker.nextId("voucherOrder"));
